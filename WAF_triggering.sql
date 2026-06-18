abde9faed1ea816835994c09e68ae94f4ca07903
Maxim <89156031+MaximKoblyuk@users.noreply.github.com>
2026-06-18 10:16:22 +0200

    Update WAF_triggering.sql

--- WAF_triggering.sql
+++ WAF_triggering.sql
@@ -1,6 +1,5 @@
 AzureDiagnostics
 | where ResourceProvider == "MICROSOFT.CDN"
     and Category == "FrontDoorWebApplicationFirewallLog"
-| where requestUri_s contains "redberry.app.euccz.org"
 | summarize Count = count() by Rule = ruleName_s, Action = action_s
 | order by Count desc
