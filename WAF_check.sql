a7c9d49f9ecf2240cb27c27d074475a32a034401
Maxim <89156031+MaximKoblyuk@users.noreply.github.com>
2026-06-18 10:10:27 +0200

    Create WAF_check.sql

---
+++ WAF_check.sql
@@ -0,0 +1,14 @@
+AzureDiagnostics
+| where ResourceProvider == "MICROSOFT.CDN"
+    and Category == "FrontDoorWebApplicationFirewallLog"
+| where requestUri_s contains "redberry.app.euccz.org"
+| where action_s == "Block"
+| project TimeGenerated,
+    ClientIP   = clientIP_s,
+    URI        = requestUri_s,
+    Rule       = ruleName_s,
+    Reason     = details_msg_s,
+    Data       = details_data_s,
+    PolicyMode = policyMode_s,
+    Ref        = trackingReference_s
+| order by TimeGenerated desc
